ba6fc6e108905c416fe5da556960c170875a8a40
sm.mostafajamal <sm.mostafajamal@gmail.com>
2021-09-24 10:31:07 +0600

    completed

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
 // create function computerPlay that randomly returns r-p-s
 function computerPlay(){
     // this returns 0-2
-    randomNum = Math.floor(Math.random()*3) 
+    randomNum = Math.floor(Math.random()*3); 
     // condition for RPS and returns the value
     return (randomNum === 0) ? 'rock'
         : (randomNum === 1) ? 'paper'
         : 'sissor';
-}
+};
+
 function playerSelected(value){
     return (value === 'rock' || value === 'paper' || value === 'sissor') ? value
-            // : undefined;
-            : false
-            
-}
-// funtion which decide if player lose or win
-function playRounds(playerSelection, computerSelection){
-    // some condition with some logical operators
-    return (playerSelection === 'rock' && computerSelection === 'paper') ?
-            console.log('You Lose! Paper beats Rock')
-            :(playerSelection === 'paper' && computerSelection === 'sissor') ?
-            console.log('You Lose! Sissor beats paper')
-            :(playerSelection === 'sissor' && computerSelection === 'rock') ?
-            console.log('You Lose! Rock beats Sissor')
-            :(playerSelection === 'paper' && computerSelection === 'rock') || 
-            (playerSelection === 'sissor' && computerSelection === 'paper') ||
-            (playerSelection === 'rock' && computerSelection === 'sissor')  ?
-            console.log('You Win')
-            :(playerSelection === false) ?
-            console.error('please enter correct value!!!') 
-            : console.log("It's a Draw")
+        : false;
+};
+
+function game (){
+    // this function have 2 variable each adds points if it wins
+    player = 0;
+    computer = 0;
+    // if any of the point become 5 while loop become false and it breaks the loop
+    while(player != 5 && computer != 5){
+        playerOption = String(prompt("Rock, Paper or Sissor???")).toLowerCase();
+        // funtion which decide if player lose or win
+        function playRounds(playerSelection, computerSelection){
+            if(playerSelection === 'rock' && computerSelection === 'paper') {
+                console.log('You Lose! Paper beats Rock'); 
+                return computer += 1; 
+            }else if(playerSelection === 'paper' && computerSelection === 'sissor'){
+                console.log('You Lose! Sissor beats paper');
+                 return computer +=1;
+            }else if(playerSelection === 'sissor' && computerSelection === 'rock') {
+                console.log('You Lose! Rock beats Sissor');
+                return computer +=1;    
+            }else if((playerSelection === 'paper' && computerSelection === 'rock') || 
+                    (playerSelection === 'sissor' && computerSelection === 'paper') ||
+                    (playerSelection === 'rock' && computerSelection === 'sissor')){
+                        console.log('You Win');
+                        return player += 1;
+            }else if(playerSelection === false) {
+                console.error('please enter correct value!!!') 
+            }else {
+                console.log("It's a Draw");
+            }    
+        
+        }
+        playRounds(playerSelected(playerOption),computerPlay());
+        console.log(`Player point is: ${player} & computers point is: ${computer}`)
+    }          
 }
 
-// prompt() for player option which receives string only
-playerOption = String(prompt("Rock, Paper or Sissor???")).toLowerCase()
-
-playRounds(playerSelected(playerOption),computerPlay())
-// Make your function’s playerSelection parameter case-insensitive
-// you want to return the results of this function call, not console.log() them. 
-// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game 
-// that keeps score and reports a winner or loser at the end. 
+game();
 
