1005d23a9c8a55da57c1ac5cc82b90a700ff4926
sm.mostafajamal <sm.mostafajamal@gmail.com>
2021-11-21 00:42:29 +0600

    score workable

--- app.js
+++ app.js
@@ -1,52 +1,42 @@
-let rock = document.querySelector('#rock-btn').addEventListener('click', function(e){
-    return playRounds(e.target.value, computerPlay);
+const rock = document.querySelector('#rock-btn').addEventListener('click', function(e){
+    return playRounds(e.target.value, computerPlay());
 });
-let paper = document.querySelector('#paper-btn').addEventListener('click', function(e){
-    return playRounds(e.target.value, computerPlay);
+const paper = document.querySelector('#paper-btn').addEventListener('click', function(e){
+    return playRounds(e.target.value, computerPlay());
 });
-let sissor = document.querySelector('#sissor-btn').addEventListener('click', function(e){
-    return playRounds(e.target.value, computerPlay);
+const sissor = document.querySelector('#sissor-btn').addEventListener('click', function(e){
+    return playRounds(e.target.value, computerPlay());
 });
 
-// this function have 2 variable each adds points if it wins
-player = 0;
-computer = 0;
-// if any of the point become 5 while loop become false and it breaks the loop
-
+let playerScore = 0;
+let computerScore = 0
+const player = document.querySelector('#player-score');
+const computer = document.querySelector('#computer-score');
 // computerPlay that randomly returns r-p-s & which corresponds to 0-2
-const computerPlay = (function(){
+function computerPlay(){
     randomNum = Math.floor(Math.random()*3); 
     // condition for RPS and returns the value
     return (randomNum === 0) ? 'rock'
-        : (randomNum === 1) ? 'paper'
-        : 'sissor';
-    })();
-    
-    
+         : (randomNum === 1) ? 'paper'
+         : 'sissor';
+};
+
 // funtion which decide if player lose or win
 function playRounds(playerSelection, computerSelection){
     console.log(playerSelection, computerSelection)
     if(playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You Lose! Paper beats Rock'); 
-        return computer += 1; 
+        return computer.textContent = ++computerScore; 
     }else if(playerSelection === 'paper' && computerSelection === 'sissor'){
-        console.log('You Lose! Sissor beats paper');
-            return computer +=1;
+        return computer.textContent = ++computerScore; 
     }else if(playerSelection === 'sissor' && computerSelection === 'rock') {
-        console.log('You Lose! Rock beats Sissor');
-        return computer +=1;    
+        return computer.textContent = ++computerScore; 
     }else if((playerSelection === 'paper' && computerSelection === 'rock') || 
             (playerSelection === 'sissor' && computerSelection === 'paper') ||
             (playerSelection === 'rock' && computerSelection === 'sissor')){
-                console.log('You Win');
-                return player += 1;
-    }else if(playerSelection === false) {
-        console.error('please enter correct value!!!') 
+        return player.textContent = ++playerScore; 
     }else {
         console.log("It's a Draw");
     }    
 }
 
-
-console.log(`Player point is: ${player} & computers point is: ${computer}`)
 
