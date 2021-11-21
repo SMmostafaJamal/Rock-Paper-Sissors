e083ae1ec4d6f4d8a032a6eef06cf136f47c4851
sm.mostafajamal <sm.mostafajamal@gmail.com>
2021-11-22 03:33:00 +0600

    done

--- app.js
+++ app.js
@@ -1,73 +1,82 @@
 const computerHand = document.querySelector('#computer-sign');
 const playerHand = document.querySelector('#player-sign');
+const message = document.querySelector('#message');
 
-
-
-
-function playerChange(sign){
+function playerPicChange(sign){
     playerHand.src = `./images/${sign}.png`;
 }
-function computerChange(sign){
+function computerPicChange(sign){
     computerHand.src = `./images/${sign}.png`
 }
 
-
 const rock = document.querySelector('#rock-btn').addEventListener('click', function(e){
-    playerChange(e.currentTarget.value);
+    playerPicChange(e.currentTarget.value);
     return playRounds(e.currentTarget.value, computerPlay()); 
     // currentTarget to work the button where ever i click inside the button
 });
 const paper = document.querySelector('#paper-btn').addEventListener('click', function(e){
-    playerChange(e.currentTarget.value);
+    playerPicChange(e.currentTarget.value);
     return playRounds(e.currentTarget.value, computerPlay());
 });
 const scissor = document.querySelector('#scissor-btn').addEventListener('click', function(e){
-    playerChange(e.currentTarget.value);
+    playerPicChange(e.currentTarget.value);
     return playRounds(e.currentTarget.value, computerPlay());
 });
-
 
 let playerScore = 0;
 let computerScore = 0;
 const player = document.querySelector('#player-score');
 const computer = document.querySelector('#computer-score');
+
+const gameOver = () => {
+    if(playerScore === 5){
+        reset();
+        message.setAttribute('style','color: green')
+        message.innerText = ' Winner !!!'
+        return
+    }else if(computerScore === 5){
+        reset()
+        message.setAttribute('style','color: red')
+        message.innerText = 'You Lose!!!'
+        return
+    }
+}
+// To reset after game is over
+const reset = () =>{
+    computerScore = 0;
+    playerScore = 0;
+    player.textContent = playerScore;
+    computer.textContent = computerScore;
+}
+
 // computerPlay that randomly returns r-p-s & which corresponds to 0-2
 function computerPlay(){
     randomNum = Math.floor(Math.random()*3); 
+    computerPicChange(randomNum)
     // condition for RPS and returns the value
-    computerChange(randomNum)
     return (randomNum === 0) ? 'rock' 
          : (randomNum === 1) ? 'paper'
          : 'scissor';
 };
-
 // funtion which decide if player lose or win
 function playRounds(playerSelection, computerSelection){
-    console.log(playerSelection, computerSelection)
-        if((playerScore !== 5) || (computerScore !== 5) && !(confirm("You Won"))){
-            if(playerSelection === 'rock' && computerSelection === 'paper') {
+    message.innerText =' ';
+    gameOver()
+    if(playerSelection === 'rock' && computerSelection === 'paper') {
+        //   message.innerText = 'Paper beats Rock';
+        return computer.textContent = ++computerScore; 
+    }else if(playerSelection === 'paper' && computerSelection === 'scissor'){
+        // message.innerText = 'Sissor beats paper';
+        return computer.textContent = ++computerScore; 
+    }else if(playerSelection === 'scissor' && computerSelection === 'rock') {
+        // message.innerText = 'Rock beats Sissor';
+        return computer.textContent = ++computerScore; 
+    }else if((playerSelection === 'paper' && computerSelection === 'rock') || 
+            (playerSelection === 'scissor' && computerSelection === 'paper') ||
+            (playerSelection === 'rock' && computerSelection === 'scissor')){
+            // message.innerText = 'You Got the point';
+            return player.textContent = ++playerScore; 
+    }
 
-                return computer.textContent = ++computerScore; 
-            }else if(playerSelection === 'paper' && computerSelection === 'scissor'){
-                return computer.textContent = ++computerScore; 
-            }else if(playerSelection === 'scissor' && computerSelection === 'rock') {
-                return computer.textContent = ++computerScore; 
-            }else if((playerSelection === 'paper' && computerSelection === 'rock') || 
-                    (playerSelection === 'scissor' && computerSelection === 'paper') ||
-                    (playerSelection === 'rock' && computerSelection === 'scissor')){
-                return player.textContent = ++playerScore; 
-            }else {
-                console.log("It's a Draw");
-            }
-        }else{
-            return reset();
-        } 
 }
 
-// To reset after game is over
-const reset = () =>{
-    playerScore = 0;
-    computerScore = 0;
-    player.textContent = playerScore;
-    computer.textContent = computerScore;
-}
